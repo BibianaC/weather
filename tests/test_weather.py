d39bef69a2e7cf795b02fc43881512b91fe36d98
bibiana <bcristofol@gmail.com>
2016-07-24 12:08:39 +0100

    Add aggreated weather data

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,4 +1,16 @@
-from weather.weather.views import temperature
+from weather.weather.views import aggreagate, average, median, temperature
+
+
+def test_average():
+    data = [18.75, 20.50, 27.01, 24.5, 26.03]
+    avg = average(data)
+    assert(avg) == 23.36
+
+
+def test_median():
+    data = [18.75, 20.50, 27.01, 24.5, 26.03]
+    med = median(data)
+    assert(med) == 24.5
 
 
 def test_temperature():
@@ -8,3 +20,40 @@ def test_temperature():
     temp = temperature(temp_info)
     result = {'min': 18.89, 'max': 27.48, 'avg': 23.19}
     assert(temp) == result
+
+
+def test_aggreagate():
+    forecast = [
+        {
+            'day': '2016-07-24 18:00:00+00',
+            'temperature': {'max': 33.1, 'avg': 29.11, 'min': 25.12},
+            'humidity': 86
+        },
+        {
+            'day': '2016-07-25 18:00:00+00',
+            'temperature': {'max': 32.46, 'avg': 29.28, 'min': 26.1},
+            'humidity': 84
+        },
+        {
+            'day': '2016-07-26 18:00:00+00',
+            'temperature': {'max': 31.55, 'avg': 28.19, 'min': 24.83},
+            'humidity': 0
+        },
+    ]
+    aggreagated = aggreagate(forecast)
+    result = {
+        'min': 24.83,
+        'max': 33.1,
+        'min_avg': 25.35,
+        'max_avg': 32.37,
+        'avg': 28.86,
+        'min_median': 25.12,
+        'max_median': 32.46,
+        'median': 29.11,
+        'min_humidity': 0,
+        'max_humidity': 86,
+        'avg_humidity': 56.67,
+        'median_humidity': 84,
+    }
+
+    assert(aggreagated) == result
--- weather/weather/views.py
+++ weather/weather/views.py
@@ -7,12 +7,21 @@ from rest_framework.response import Response
 from settings import WEATHER_API_KEY
 
 
+def average(list_num):
+    avg = numpy.mean(list_num)
+    return round(avg, 2)
+
+
+def median(list_num):
+    median = numpy.median(list_num)
+    return round(median, 2)
+
+
 def temperature(day_temp):
     min_temp = day_temp['min']
     max_temp = day_temp['max']
-    avg_temp = numpy.mean([min_temp, max_temp])
-    avg_round = round(avg_temp, 2)
-    temperature = {'min': min_temp, 'max': max_temp, 'avg': avg_round}
+    avg_temp = average([min_temp, max_temp])
+    temperature = {'min': min_temp, 'max': max_temp, 'avg': avg_temp}
     return temperature
 
 
@@ -39,9 +48,45 @@ def get_forecast(location, days):
     return forecast(forecast_data)
 
 
+def aggreagate(forecast):
+    daily_min = []
+    daily_max = []
+    daily_avg = []
+    daily_humidity = []
+
+    for info in forecast:
+        temperature = info['temperature']
+        daily_min.append(temperature['min'])
+        daily_max.append(temperature['max'])
+        daily_avg.append(temperature['avg'])
+        daily_humidity.append(info['humidity'])
+
+    aggregated = {
+        'min': min(daily_min),
+        'max': max(daily_max),
+        'min_avg': average(daily_min),
+        'max_avg': average(daily_max),
+        'avg': average(daily_avg),
+        'min_median': median(daily_min),
+        'max_median': median(daily_max),
+        'median': median(daily_avg),
+        'min_humidity': min(daily_humidity),
+        'max_humidity': max(daily_humidity),
+        'avg_humidity': average(daily_humidity),
+        'median_humidity': median(daily_humidity),
+    }
+
+    return aggregated
+
+
 def weather_info(location, days):
     forecast = get_forecast(location, days)
-    return forecast
+    aggregated = aggreagate(forecast)
+    weather = {
+        'daily_forecast': forecast,
+        'aggregated': aggregated,
+    }
+    return weather
 
 
 @api_view(['GET'])
