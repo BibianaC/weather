d7f53ec507c9c4a1a87154109d628c95047637dc
bibiana <bcristofol@gmail.com>
2016-07-24 11:23:25 +0100

    Refactor views

--- weather/weather/views.py
+++ weather/weather/views.py
@@ -7,39 +7,44 @@ from rest_framework.response import Response
 from settings import WEATHER_API_KEY
 
 
-location = 'London,uk'
-
-
-def temperature(temperature_data):
-    print temperature_data
-    min_temp = temperature_data['min']
-    max_temp = temperature_data['max']
+def temperature(day_temp):
+    min_temp = day_temp['min']
+    max_temp = day_temp['max']
     avg_temp = numpy.mean([min_temp, max_temp])
     avg_round = round(avg_temp, 2)
     temperature = {'min': min_temp, 'max': max_temp, 'avg': avg_round}
     return temperature
 
 
-def get_weather(location, days=7):
-    owm = OWM(WEATHER_API_KEY)
-    forecast = owm.daily_forecast(location, limit=days)
-    get_forecast = forecast.get_forecast()
-
-    weather = []
-    for info in get_forecast:
-        day = info.get_reference_time('iso')
-        humidity = info.get_humidity()
-        temp_info = info.get_temperature('celsius')
-        temp = temperature(temp_info)
-        weather.append({
+def forecast(forecast_data):
+    forecast = []
+    for day_weather in forecast_data:
+        day = day_weather.get_reference_time('iso')
+        humidity = day_weather.get_humidity()
+        day_temp = day_weather.get_temperature('celsius')
+        temp = temperature(day_temp)
+        forecast.append({
             'day': day,
             'humidity': humidity,
             'temperature': temp,
         })
 
-    return weather
+    return forecast
+
+
+def get_forecast(location, days):
+    owm = OWM(WEATHER_API_KEY)
+    daily_forecast = owm.daily_forecast(location, limit=days)
+    forecast_data = daily_forecast.get_forecast()
+    return forecast(forecast_data)
+
+
+def weather_info(location, days):
+    forecast = get_forecast(location, days)
+    return forecast
 
 
 @api_view(['GET'])
-def weather(request):
-    return Response(get_weather(location, 7))
+def weather(request, location, days_str):
+    days = int(days_str)
+    return Response(weather_info(location, days))
